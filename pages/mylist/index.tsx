e4824ed9c2ffc38ca085ba3cf4dd8760cdff75c8
Tumelo Koko <tumelokoko10@gmail.com>
2022-10-28 14:31:10 +0200

    Update: Add functionality to back button

--- pages/mylist/index.tsx
+++ pages/mylist/index.tsx
@@ -1,29 +1,31 @@
 import Image from "next/image"
 import Link from "next/link"
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/List.module.css'
 
 const SearchBar = () => {
-    return <input type="text" placeholder="Search..." />
+    return <input className={styles.search} type="text" placeholder="Search..." />
 }
 
 const Menu = () => {
     let iconSize = 32
-    return <button><Image src="/icons/menu.svg" height={iconSize} width={iconSize} alt="" /></button>
+    return <button className={styles.action}><Image src="/icons/menu.svg" height={iconSize} width={iconSize} alt="" /></button>
 }
 
 const Back = () => {
     let iconSize = 32
-    return <button><Image src="/icons/left.svg" height={iconSize} width={iconSize} alt="" /></button>
+    return <Link href="/" className={styles.action}><Image src="/icons/left.svg" height={iconSize} width={iconSize} alt="" /></Link>
 }
 
 export default function Mylist() {
-    return <div className={styles.main}>
-        <div className={styles.nav}>
-            <Back />
-            <SearchBar />
-            <Menu />
-        </div>
-        <div>My List</div>
-        <Link href="/">Home</Link>
-    </div>
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <div className={styles.nav}>
+                    <Back />
+                    <SearchBar />
+                    <Menu />
+                </div>
+                <div className={styles.heading}>My List</div>
+            </div>
+        </div>)
 }
