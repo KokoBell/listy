0f15bf0b281e429e9e5ae9d87ec67d0570953bf5
KokoBell <tumelokoko10@gmail.com>
2023-01-03 22:57:16 +0200

    Upd: create logout button

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,13 +1,22 @@
-
+import supabase from '../supabase'
 import inputProps from '../interfaces/inputProps'
 import styles from '../styles/List.module.css'
 
 const Toolbar = ({ open, setOpen }: inputProps) => {
+    const logout = async () => {
+        try {
+            const { error } = await supabase.auth.signOut()
+        } catch (error: any) {
+            console.error(error.message)
+        }
+    }
     return <div className={styles.toolbar}>
-        <div className={styles.tool_action}>Home</div>
-        <div className={styles.tool_action} onClick={() => setOpen(!open)}>Add</div>
-        <div className={styles.tool_action}>Profile</div>
-    </div>
+        <button className={styles.tool_action} onClick={async () => {
+            await logout()
+        }}>Logout</button>
+        <button className={styles.tool_action} onClick={() => setOpen(!open)}>Add</button>
+        <button className={styles.tool_action}>Feedback</button>
+    </div >
 }
 
 export default Toolbar
